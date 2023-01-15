267c5e2e412b91b18139c067f7288bf9bd424699
Rono-Koushique <rono2170@gmail.com>
2023-01-16 01:27:39 +0600

    hero image priority

--- components/slides/HeroSlide.tsx
+++ components/slides/HeroSlide.tsx
@@ -9,12 +9,17 @@ interface Props {
 export default function HeroSlide({ imgSrc, title }: Props) {
     return (
         <div className="h-full w-full relative">
-            <Image className="h-full object-cover" src={imgSrc} alt="" />
-            <div className="absolute top-0 left-1/2 h-full w-full max-w-6xl 
-                        flex flex-col justify-between -translate-x-1/2 px-12 py-20">
-                <h1
-                    className="text-3xl text-white w-1/2 text-left font-semibold"
-                >
+            <Image
+                className="h-full object-cover"
+                src={imgSrc}
+                alt=""
+                priority={true}
+            />
+            <div
+                className="absolute top-0 left-1/2 h-full w-full max-w-6xl 
+                        flex flex-col justify-between -translate-x-1/2 px-12 py-20"
+            >
+                <h1 className="text-3xl text-white w-1/2 text-left font-semibold">
                     {title}
                 </h1>
                 <h2
